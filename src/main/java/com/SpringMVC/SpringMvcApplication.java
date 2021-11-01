8711f9faae5c49181927035e35a037313df10ede
Ugur Askar <ugurasker@gmail.com>
2021-11-01 12:27:08 +0100

    SpringMVC examples

---
+++ src/main/java/com/SpringMVC/SpringMvcApplication.java
@@ -0,0 +1,15 @@
+package com.SpringMVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan({"controller"})
+public class SpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcApplication.class, args);
+	}
+
+}
---
+++ src/main/webapp/sonuc.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Sonuclar</title>
+</head>
+<body>
+   <h1>Sonuclar</h1>
+   <p> Girilen Sayilarin Toplami: ${sayilarinToplami} <p>
+    <p> Girilen Sayilarin Carpimi: ${sayilarinCarpimi} <p>
+</body>
+</html>
